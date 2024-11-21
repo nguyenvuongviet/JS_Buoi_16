12f265f212e5edfe1c36882a0712df4c812573f5
nguyenvuongviet <nguyenvuongviet2k4@gmail.com>
2024-11-21 22:17:04 +0700

    final

--- main.js
+++ main.js
@@ -3,17 +3,20 @@ let array = [];
 function addToArray() {
   const input = document.getElementById("inputArray").value;
   array = input.split(",").map(Number);
+
   result = `Mảng sau khi nhập thêm: ${array}`;
   document.getElementById("addToArray").innerHTML = result;
 }
 
 function sumPositive() {
   let sum = 0;
+
   for (let i = 0; i < array.length; i++) {
     if (array[i] > 0) {
       sum += array[i];
     }
   }
+
   result = `<p>Mảng: ${array}</p>
   Tổng các số dương trong mảng là: ${sum}`;
   document.getElementById("sumPositive").innerHTML = result;
@@ -21,11 +24,13 @@ function sumPositive() {
 
 function countPositive() {
   let count = 0;
+
   for (let i = 0; i < array.length; i++) {
     if (array[i] > 0) {
       count++;
     }
   }
+
   result = `<p>Mảng: ${array}</p>
   Số dương trong mảng là: ${count}`;
   document.getElementById("countPositive").innerHTML = result;
@@ -33,11 +38,13 @@ function countPositive() {
 
 function findMin() {
   let min = array[0];
+
   for (let i = 1; i < array.length; i++) {
     if (array[i] < min) {
       min = array[i];
     }
   }
+
   result = `<p>Mảng: ${array}</p>
   Số nhỏ nhất trong mảng là: ${min}`;
   document.getElementById("findMin").innerHTML = result;
@@ -45,11 +52,13 @@ function findMin() {
 
 function findMinPositive() {
   let minPositive = null;
+
   for (let i = 0; i < array.length; i++) {
     if (array[i] > 0 && (minPositive === null || array[i] < minPositive)) {
       minPositive = array[i];
     }
   }
+
   result = `<p>Mảng: ${array}</p>
   Số dương nhỏ nhất trong mảng là: ${minPositive}`;
   document.getElementById("findMinPositive").innerHTML = result;
@@ -57,12 +66,14 @@ function findMinPositive() {
 
 function findLastEven() {
   let lastEven = -1;
+
   for (let i = array.length - 1; i >= 0; i--) {
     if (array[i] % 2 === 0) {
       lastEven = array[i];
       break;
     }
   }
+
   result = `<p>Mảng: ${array}</p>
   Số chẵn cuối cùng trong mảng là: ${lastEven}`;
   document.getElementById("findLastEven").innerHTML = result;
@@ -73,11 +84,11 @@ function swapPosition() {
   const position2 = parseInt(document.getElementById("position2").value);
   let result = `<p>Mảng: ${array}</p>`;
   let array1 = array.slice();
-  console.log(array1);
+
   const temp = array1[position1];
   array1[position1] = array1[position2];
   array1[position2] = temp;
-  console.log(array);
+
   result += `Mảng sau khi đổi vị trí: ${array1}`;
   document.getElementById("swapPosition").innerHTML = result;
 }
@@ -85,6 +96,7 @@ function swapPosition() {
 function sortAscending() {
   result = `<p>Mảng: ${array}</p>`;
   let array1 = array.slice();
+
   array1.sort((a, b) => a - b);
   result += ` Mảng sau khi sắp xếp tăng dần: ${array1}`;
   document.getElementById("sortAscending").innerHTML = result;
@@ -94,22 +106,26 @@ function isPrime(num) {
   if (num <= 1) {
     return false;
   }
+
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
       return false;
     }
   }
+
   return true;
 }
 
 function firstPrime() {
   let firstPrime = -1;
+
   for (let i = 0; i < array.length; i++) {
     if (array[i] > 0 && isPrime(array[i])) {
       firstPrime = array[i];
       break;
     }
   }
+
   result = `<p>Mảng: ${array}</p>
   Số nguyên tố đầu tiên trong mảng là: ${firstPrime}`;
   document.getElementById("firstPrime").innerHTML = result;
@@ -118,8 +134,10 @@ function firstPrime() {
 function countPrime() {
   const input = document.getElementById("inputArray2").value;
   const newArray = input.split(",").map(Number);
+
   array = array.concat(newArray);
   const count = array.filter((num) => Number.isInteger(num)).length;
+
   result = `<p>Mảng sau khi nhập thêm: ${array}</p>
   Số lượng số nguyên trong mảng là: ${count}`;
   document.getElementById("countPrime").innerHTML = result;
@@ -128,6 +146,7 @@ function countPrime() {
 function comparePosNeg() {
   let positiveCount = 0;
   let negativeCount = 0;
+
   for (let i = 0; i < array.length; i++) {
     if (array[i] > 0) {
       positiveCount++;
@@ -135,6 +154,7 @@ function comparePosNeg() {
       negativeCount++;
     }
   }
+
   result = `<p>Mảng: ${array}</p>
   Số lượng số dương trong mảng là: ${positiveCount}, số lượng số ám trong mảng là: ${negativeCount}`;
   document.getElementById("comparePosNeg").innerHTML = result;
